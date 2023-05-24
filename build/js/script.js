4cf86448fa40068e275b5870d87c900e364effb1
Jhnss25 <jhnsaave1997@gmail.com>
2023-05-24 11:26:06 -0500

    inciando con js

---
+++ build/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formulario = document.querySelector('.formulario');
+    const textoIngresado = document.querySelector('.formulario-propina');
+
+    textoIngresado.addEventListener();
+
+});
