83eded46fa64def799dd57027d643b23abc96e33
Jhnss25 <jhnsaave1997@gmail.com>
2023-05-25 13:49:19 -0500

    Toca arreglar mi error

--- Calculadora_de_propinas/build/js/script.js
+++ Calculadora_de_propinas/build/js/script.js
@@ -1,10 +1,13 @@
 /* Variables */
 const formulario = document.querySelector('.formulario');
+// inputs
 const inputMontoCuenta = document.querySelector('#ingresar-pago');
 const inputPorcentajePropina = document.querySelector('#porcentaje-propina');
 const inputPersonas = document.querySelector('#personas');
+// botones
 const btnCalcular = document.querySelector('#calcular');
 const btnReset = document.querySelector('#reset');
+// valores de los inputs
 const valorInputs = {
     montoCuenta: '',
     porcentajePropina: '',
@@ -14,18 +17,22 @@ const valorInputs = {
 /* Eventos */
 // btnReset.addEventListener('click', resetarInformacion);
 btnCalcular.addEventListener('click', calcularPropina);
-inputMontoCuenta.addEventListener('blur', rellenarInputs);
-inputPorcentajePropina.addEventListener('blur', rellenarInputs);
-inputPersonas.addEventListener('blur', rellenarInputs);
 
 /* Funciones */
 function calcularPropina(e) {
-
     e.preventDefault();
+    console.log('hola...');
     
-
+    if (inputMontoCuenta.id === 'ingresar-pago') {
+        valorInputs.montoCuenta = e.target.value;
+    } else if (inputPorcentajePropina.id === 'porcentaje-propina') {
+        valorInputs.porcentajePropina = e.target.value;
+    } else if (inputPersonas.id === 'personas') {
+        valorInputs.personas = e.target.value;
+    }
     // console.log(typeof inputMontoCuenta.value);
 
+    console.log(valorInputs);
     
 }
 
@@ -35,17 +42,6 @@ function calcularPropina(e) {
     //     console.log(formulario.nombre);
     //     console.log('Estoy reseteando el texto...');
 // }
-
-/* Rellena el objeto de los inputs */
-function rellenarInputs(e) {
-    if (e.target.id === 'ingresar-pago') {
-        valorInputs.montoCuenta = e.target.value;
-    } else if (e.target.id === 'porcentaje-propina') {
-        valorInputs.porcentajePropina = e.target.value;
-    } else if (e.target.id === 'personas') {
-        valorInputs.personas = e.target.value;
-    }
-}
 
 // Crea el parrafo para ingresar el mensaje
 function crearParrafo(mensaje, clase) {
